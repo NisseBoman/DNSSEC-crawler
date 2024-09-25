32adb205c789962423bfc80f8f609f809ba042e8
NisseBoman <nisse@tnfgruppen.com>
2024-09-25 10:53:59 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+#api_url = "https://jsonplaceholder.typicode.com/todos/1"
+#response = requests.get(api_url)
+#response.json()
+
+#command to check DNS  whois www.ikea.com  | egrep -i "DNSSEC|signed"
+
+#1. read a list of CID's
+#2. loop the services on each CID to get a list of domains
+#3. check each domain
+#4. output report 
+
+
